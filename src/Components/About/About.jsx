680a45c50b4409b3ff7722ad23bfe5e476451521
RaphaelFiais <devfiais23@gmail.com>
2023-01-20 11:36:48 -0300

    Ajuste de foto no about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../Title/Title'
 import { SobreMim, TextAbout } from './style'
-
+import {MyPhoto} from '../../assets/pngegg.png'
 function About() {
     return (
 
@@ -10,7 +10,7 @@ function About() {
             <Title>Sobre mim</Title>
             
                 <TextAbout>
-                <img src="./src/assets/pngegg.png" alt="" />
+                <img src={MyPhoto} alt="" />
                     <p>
                         Como Desenvolvedor front-end, tenho habilidades em HTML, CSS, JavaScript, ReactJs e NodeJs.
                         Gosto de trabalhar em equipe e sempre estou disponível para aprender mais e evoluir minhas habilidades.
